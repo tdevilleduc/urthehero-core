d54272c71988ee9d550b0585c973129920428318
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2020-01-29 10:46:27 +0100

    clean project and use only helloService
    The point is to move slowly every core function to core project

--- src/main/java/com/tdevilleduc/urthehero/core/service/HelloService.java
+++ src/main/java/com/tdevilleduc/urthehero/core/service/HelloService.java
@@ -1,16 +1,18 @@
 package com.tdevilleduc.urthehero.core.service;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.springframework.stereotype.Service;
 
-import javax.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
+@Service
 public class HelloService {
 
-    @ConfigProperty(name = "greeting")
-    private String greeting;
+//    @ConfigProperty(name = "greeting")
+//    private String greeting;
 
-    public String politeHello(String name){
-        return greeting + " " + name;
+//    public String politeHello(String name){
+//        return greeting + " " + name;
+//    }
+
+    public String hello(String name){
+        return "Hello " + name;
     }
 }
