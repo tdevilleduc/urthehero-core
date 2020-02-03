92e5b0f4b299124d2e108ab9c9602987ceb2ae26
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2020-02-03 10:42:46 +0100

    [feat] add politeHello service method
    with @configProperty

--- src/main/java/com/tdevilleduc/urthehero/core/service/HelloService.java
+++ src/main/java/com/tdevilleduc/urthehero/core/service/HelloService.java
@@ -1,16 +1,17 @@
 package com.tdevilleduc.urthehero.core.service;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloService {
 
-//    @ConfigProperty(name = "greeting")
-//    private String greeting;
+    @ConfigProperty(name = "greeting")
+    private String greeting;
 
-//    public String politeHello(String name){
-//        return greeting + " " + name;
-//    }
+    public String politeHello(String name){
+        return greeting + " " + name;
+    }
 
     public String hello(String name){
         return "Hello " + name;
