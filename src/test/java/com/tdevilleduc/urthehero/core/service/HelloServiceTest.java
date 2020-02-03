669b4b0b7964714d474d678c646fae237e7e0ba8
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2020-02-03 10:55:04 +0100

    [test] add some tests for helloService

---
+++ src/test/java/com/tdevilleduc/urthehero/core/service/HelloServiceTest.java
@@ -0,0 +1,29 @@
+package com.tdevilleduc.urthehero.core.service;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@QuarkusTest
+public class HelloServiceTest {
+
+    @Autowired
+    HelloService helloService;
+
+    private static final String name = "toto";
+
+    @Test
+    public void hello_thenSuccess() {
+        String result = helloService.hello(name);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals("Hello " + name, result);
+    }
+
+    @Test
+    public void politeHello_thenSuccess() {
+        String result = helloService.politeHello(name);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals("Good afternoon " + name, result);
+    }
+}
